66a3c05594fe9a507b7b4ce1863520fcc380831b
shuix007 <shuix007@umn.edu>
2024-01-08 23:56:46 -0600

    Update parameters.

--- meta_submit.sh
+++ meta_submit.sh
@@ -1,10 +1,30 @@
 #!/bin/bash -l
 
 declare -a seed_list=("1" "2" "3")
-declare -a data_list=("kim" "acl" "scicite" "kim-acl" "kim-scicite" "acl-scicite" "acl-kim" "scicite-kim" "scicite-acl" "acl-kim-scicite" "kim-acl-scicite" "scicite-acl-kim")
+declare -a double_data_list=("kim-acl" "kim-scicite" "acl-scicite" "acl-kim" "scicite-kim" "scicite-acl")
+declare -a triple_data_list=("acl-kim-scicite" "kim-acl-scicite" "scicite-acl-kim")
+declare -a lambda_list=("0.2" "0.4" "0.6" "0.8" "1.0")
 
 for seed in ${seed_list[@]}; do
-for data in ${data_list[@]}; do
-    sbatch slurm.sh -d ${data} -s ${seed} -p CL
+for data in ${double_data_list[@]}; do
+for lmbdas in ${lambda_list[@]}; do
+    sbatch slurm.sh \
+        -d ${data} \
+        -s ${seed} \
+        -p "1"-${lmbdas}
+done
+done
+done
+
+for seed in ${seed_list[@]}; do
+for data in ${double_data_list[@]}; do
+for lmbdas1 in ${lambda_list[@]}; do
+for lmbdas2 in ${lambda_list[@]}; do
+    sbatch slurm.sh \
+        -d ${data} \
+        -s ${seed} \
+        -p ${lmbdas1}-${lmbdas2}
+done
+done
 done
 done
--- slurm.sh
+++ slurm.sh
@@ -6,7 +6,7 @@
 #SBATCH --output=result-%j.out
 #SBATCH --error=result-%j.err
 
-#SBATCH --time=4:00:00
+#SBATCH --time=1:00:00
 
 #SBATCH --ntasks=2
 
@@ -20,7 +20,7 @@ do
     case "${flag}" in
         s) seed=${OPTARG};;
         d) data=${OPTARG};;
-        p) pl=${OPTARG};;
+        p) lmbdas=${OPTARG};;
     esac
 done
 
@@ -32,7 +32,16 @@ CURRENTEPOCTIME=`date +"%Y-%m-%d-%H-%M-%S"`
 # Run the PyTorch training script
 python main.py \
     --dataset=${data} \
+    --lmbdas=${lmbdas} \
     --data_dir=Data \
-    --workspace=Workspaces/${CURRENTEPOCTIME}-${data}-${seed}-${pl}-scibert \
-    --seed=${seed} \
-    --pl=${pl} &> ${CURRENTEPOCTIME}-${data}-${seed}-${pl}-scibert-log.txt
+    --lm=scibert \
+    --workspace=Workspaces/${CURRENTEPOCTIME}-${data}-${seed}-${lmbdas}-scibert \
+    --seed=${seed} &> ${CURRENTEPOCTIME}-${data}-${seed}-${lmbdas}-scibert-log.txt
+
+python main.py \
+    --dataset=${data} \
+    --lmbdas=${lmbdas} \
+    --data_dir=Data \
+    --lm=bert \
+    --workspace=Workspaces/${CURRENTEPOCTIME}-${data}-${seed}-${lmbdas}-bert \
+    --seed=${seed} &> ${CURRENTEPOCTIME}-${data}-${seed}-${lmbdas}-bert-log.txt
