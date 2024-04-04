3f9c52de68f2a36bf969a2de95a4a214f08ce1a7
shuix007 <shuix007@umn.edu>
2024-04-03 19:05:01 -0500

    Update scicite subset experimennts

--- meta_submit_trl_scicite_005.sh
+++ meta_submit_trl_scicite_005.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
 declare -a seed_list=("1" "2" "3" "4" "5")
-declare -a double_data_list=("scicite_050-kim" "scicite_050-acl" "scicite_020-kim" "scicite_020-acl" "scicite_005-kim" "scicite_005-acl" "acl-scicite_005" "kim-scicite_005" "acl-scicite_020" "kim-scicite_020")
+declare -a double_data_list=("scicite_050-kim" "scicite_050-acl" "scicite_020-kim" "scicite_020-acl" "scicite_005-kim" "scicite_005-acl" "acl-scicite_005" "kim-scicite_005" "acl-scicite_020" "kim-scicite_020" "acl-scicite_050" "kim-scicite_050")
 declare -a triple_data_list=("scicite_050-acl-kim" "scicite_020-acl-kim" "scicite_005-acl-kim")
 
 for seed in ${seed_list[@]}; do
